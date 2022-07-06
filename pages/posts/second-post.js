f8d1751a29fe3d233a25a39b26237cb3b1fca20c
Benji <benjifriedman@gmail.com>
2022-07-06 13:56:40 -0700

    asdf

---
+++ pages/posts/second-post.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Link from "next/link";
+// import Script from "next/script";
+import Layout from "../../components/layout";
+
+export default function SecondPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>Second Post</title>
+      </Head>
+
+      <h1>Second Post</h1>
+      <p>This is also some text.</p>
+      <p>What if this was a text post?</p>
+      {/* <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2> */}
+    </Layout>
+  );
+}
