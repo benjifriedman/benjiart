0a309a04b799ca6238738eec96f4ab5ad48e47bf
Benji <benjifriedman@gmail.com>
2022-07-06 19:29:23 -0700

    asdf

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import "../styles/global.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-Y2N9S8XLP7"}
+      />
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
