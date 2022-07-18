ba4274bceda747cc7425b48200edd1efad892f8a
Benji <benjifriedman@gmail.com>
2022-07-17 18:06:06 -0700

    asdf

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+This is what a page looks like:
+
+import Head from "next/head";
+import Link from "next/link";
+// import Script from "next/script";
+import Layout from "../../components/layout";
+
+export default function SecondPost() {
+return (
+<Layout>
+<Head>
+<title>Second Post</title>
+</Head>
+
+      <h1>Second Post</h1>
+      <p>This is also some text.</p>
+      <p>What if this was a text post?</p>
+      {/* <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2> */}
+    </Layout>
+
+);
+}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Layout from "../../components/layout";
-import Date from "../../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
 
@@ -28,9 +27,6 @@ export default function Post({ postData }) {
       </Head>
       <article>
         <h1 className={utilStyles.headingLg}>{postData.title}</h1>
-        {/* <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div> */}
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
